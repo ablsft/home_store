f3db84d4ff3b9e48703be94dcbaa8d9ef09fae9d
Alexander Boriskin <iboriskin5@gmail.com>
2024-03-13 15:22:37 +0300

    'cart_add' view written; cart templates refactored

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+import re
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
@@ -17,7 +18,8 @@ def login(request):
                 auth.login(request, user)
                 messages.success(request, f'Добро пожаловать, {username}!')
 
-                if request.POST.get('next', None):
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
                 
                 return HttpResponseRedirect(reverse('main:index'))
